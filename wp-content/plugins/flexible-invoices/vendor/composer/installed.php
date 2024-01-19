bc72095df662ebf6316d0a32bf0c6031fd917f45
heng michel <cavacmoi@gmail.com>
2024-01-19 16:55:53 +0100

    3e commit

---
+++ wp-content/plugins/flexible-invoices/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpdesk/flexible-invoices',
+        'pretty_version' => '5.6.22',
+        'version' => '5.6.22.0',
+        'reference' => '6eda320da45cb3f6eb7ed49fc6964c2f9482a553',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpdesk/flexible-invoices' => array(
+            'pretty_version' => '5.6.22',
+            'version' => '5.6.22.0',
+            'reference' => '6eda320da45cb3f6eb7ed49fc6964c2f9482a553',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpdesk/wp-wpdesk-helper-override' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '77844fc71da7ebb7645f10f0fc15f5294fed8542',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpdesk/wp-wpdesk-helper-override',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
